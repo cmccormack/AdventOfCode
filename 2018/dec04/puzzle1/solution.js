8e195f43d89684dac8573feff424f3b453d32abf
cmccormack <mccormack.christopher@gmail.com>
2018-12-04 13:44:59 -0700

    Competed day 4 puzzle 1

---
+++ 2018/dec04/puzzle1/solution.js
@@ -0,0 +1,92 @@
+/*
+[1518-09-26 00:02] Guard #1901 begins shift
+[1518-08-24 00:24] falls asleep
+[1518-07-16 00:26] falls asleep
+[1518-08-19 00:19] falls asleep
+[1518-10-20 00:34] wakes up
+*/
+
+const fs = require('fs')
+
+// Trim and split text into an Array of strings
+const stringCleaning = str => str.trim().split(/\r?\n+/)
+
+const roundMinutes = date => {
+  date.setDate(date.getDate() + Math.round(date.getMinutes()/60))
+  date.setMinutes(0)
+
+  return date
+}
+
+module.exports = (str, start=0) => {
+
+  const guards = {}
+  const entries = stringCleaning(str)
+  
+  .map(entry => {
+    const month = +entry.slice(6,8) - 1
+    const day = entry.slice(9,11)
+    const min = entry.slice(15,17)
+    const date = new Date(1970, month, day, 0, min, 0)
+    const action = entry.slice(19)
+    const originalDate = entry.slice(1, 17)
+
+    return ({
+      date: action.includes('Guard') ? roundMinutes(new Date(date)) : date,
+      action,
+      originalDate,
+    })
+  })
+  .sort((a,b) => (
+    a.date.getTime() - b.date.getTime()
+  ))
+
+  let guard
+  let sleepStart
+  for (entry of entries) {
+
+    const {date, action, originalDate} = entry
+    
+    if (action.includes('Guard')) {
+      guardId = action.match(/#\d+\s+/g)[0].slice(1)
+      shiftDate = `${date.getMonth()+1}-${('0' + (date.getDate())).slice(-2)}`
+      if (!guards[guardId]) {
+        guards[guardId] = {}
+      }
+      guard = guards[guardId]
+      guard[shiftDate] = []
+      continue
+    }
+
+    if (action === 'falls asleep') {
+      sleepStart = date.getMinutes()
+    }
+
+    if (action === 'wakes up') {
+      const sleepEnd = date.getMinutes()
+      for (let i = sleepStart; i < sleepEnd; i+=1) {
+        guard[shiftDate].push(i)
+      }
+    }
+  }
+
+  // Determine the top sleeper's ID
+  const topSleeperId = Object.keys(guards).map(guard => ({
+      id: guard,
+      slept: Object.values(guards[guard]).reduce((a,v) => a += v.length, 0),
+  })).reduce((a,v) => a.slept > v.slept ? a : v).id
+
+  // Find the most slept minute for top sleeper
+  const bin = {}
+
+  for (set of Object.values(guards[topSleeperId])) {
+    for (min of set) {
+      if (!bin[min]) {
+        bin[min] = 0
+      }
+      bin[min] += 1
+    }
+  }
+
+  return Object.entries(bin).sort((a,b) => b[1] - a[1])[0][0] * topSleeperId
+}
