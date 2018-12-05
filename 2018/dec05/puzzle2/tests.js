bb1b53171e51c3afb22b6d57ce8a35fea1de64ba
Christopher McCormack <mccormack.christopher@gmail.com>
2018-12-05 08:39:30 -0700

    Completed dec05 puzzle2

---
+++ 2018/dec05/puzzle2/tests.js
@@ -0,0 +1,30 @@
+/*
+For example:
+
+In aA, a and A react, leaving nothing behind.
+In abBA, bB destroys itself, leaving aA. As above, this then destroys itself, leaving nothing.
+In abAB, no two adjacent units are of the same type, and so nothing happens.
+In aabAAB, even though aa and AA are of the same type, their polarities match, and so nothing happens.
+Now, consider a larger example, dabAcCaCBAcCcaDA:
+
+dabAcCaCBAcCcaDA  The first 'cC' is removed.
+dabAaCBAcCcaDA    This creates 'Aa', which is removed.
+dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
+dabCBAcaDA        No further actions can be taken.
+After all possible reactions, the resulting polymer contains 10 units.
+*/
+
+const assert = require('assert')
+const solution = require('./solution')
+
+const testinput =
+`
+dabAcCaCBAcCcaDA
+`
+
+describe('solution', () => {
+  it('should return the units that remain', () => {
+    assert.equal(solution(testinput), 4)
+  })
+})
+
